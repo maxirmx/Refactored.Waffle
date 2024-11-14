f08a1db8bdcf5c047d9ea18dcce646fb2e55eb9b
Maxim Samsonov <m.samsonov@computer.org>
2024-11-14 17:59:19 +0300

    feat: move in scheduler service

---
+++ Scheduler/Services/JobService.cs
@@ -0,0 +1,147 @@
+﻿// Copyright (C) 2024 Maxim [maxirmx] Samsonov (www.sw.consulting)
+// All rights reserved.
+// This file is a part of OiltrackGateway applcation
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
+// BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+using Microsoft.Extensions.Logging;
+using Quartz;
+using Refactored.Waffle.Scheduler.Jobs;
+
+namespace Refactored.Waffle.Scheduler.Services;
+
+public sealed class JobService(
+    ISchedulerFactoryService factoryService, ILogger<JobService> logger) : IJobService
+{
+    private IScheduler? _scheduler;
+
+    public async Task<bool> DoesJobExist(
+        string jobKey, CancellationToken cancellationToken)
+    {
+        logger.LogDebug("[{Name}] start", nameof(DoesJobExist));
+        _scheduler ??= await factoryService.GetScheduler(cancellationToken);
+
+        var existingJobKey = new JobKey(jobKey);
+        var exist = await _scheduler.CheckExists(existingJobKey, cancellationToken);
+        logger.LogDebug("[{Name}] finished, {IsExist}", nameof(DoesJobExist), exist);
+        return exist;
+    }
+
+    public async Task ScheduleJob<TCommand>(
+        string jobKey, string cronExpression, CancellationToken cancellationToken)
+        where TCommand : new()
+    {
+        await ScheduleJobInternal<TCommand, string>(jobKey, cronExpression, CreateTrigger, cancellationToken);
+    }
+
+    public async Task ScheduleJob<TCommand>(
+        string jobKey, int minutes, CancellationToken cancellationToken)
+        where TCommand : new()
+    {
+        await ScheduleJobInternal<TCommand, int>(jobKey, minutes, CreateTrigger, cancellationToken);
+    }
+    public async Task RescheduleJob<TCommand>(
+        string jobKey, string newCronExpression, CancellationToken cancellationToken)
+        where TCommand : new()
+    {
+        await RescheduleJobInternal<TCommand, string>(jobKey, newCronExpression, CreateTrigger, cancellationToken);
+    }
+
+    public async Task RescheduleJob<TCommand>(
+        string jobKey, int minutes, CancellationToken cancellationToken)
+        where TCommand : new()
+    {
+        await RescheduleJobInternal<TCommand, int>(jobKey, minutes, CreateTrigger, cancellationToken);
+    }
+
+    public async Task UnscheduleJob(
+        string jobKey, CancellationToken cancellationToken)
+    {
+        logger.LogDebug("[{Name}] start", nameof(UnscheduleJob));
+
+        _scheduler ??= await factoryService.GetScheduler(cancellationToken);
+
+        var triggerKey = new TriggerKey(GetIdentity(jobKey));
+        await _scheduler.UnscheduleJob(triggerKey, cancellationToken);
+
+        logger.LogDebug("[{Name}] finished", nameof(UnscheduleJob));
+    }
+
+    private async Task RescheduleJobInternal<TCommand, TSchedulingValue>(
+        string jobKey, TSchedulingValue schedulingValue,
+        Func<string, TSchedulingValue, JobKey, ITrigger> triggerCreator,
+        CancellationToken cancellationToken)
+        where TCommand : new()
+    {
+        logger.LogDebug("[{Name}] start", nameof(RescheduleJob));
+        _scheduler ??= await factoryService.GetScheduler(cancellationToken);
+
+        await UnscheduleJob(jobKey, cancellationToken);
+        await ScheduleJobInternal<TCommand, TSchedulingValue>(jobKey, schedulingValue, triggerCreator, cancellationToken);
+    
+        logger.LogDebug("[{Name}] finished", nameof(RescheduleJob));
+    }
+
+    private async Task ScheduleJobInternal<TCommand, TSchedulingValue>(
+        string jobKey, TSchedulingValue schedulingValue,
+        Func<string, TSchedulingValue, JobKey, ITrigger> triggerCreator, 
+        CancellationToken cancellationToken)
+        where TCommand : new()
+    {
+        logger.LogDebug("[{Name}] start", nameof(ScheduleJob));
+        _scheduler ??= await factoryService.GetScheduler(cancellationToken);
+
+        var key = new JobKey(jobKey);
+        var jobDetail = JobBuilder
+            .Create<SchedulingJob<TCommand>>()
+            .WithIdentity(key)
+            .Build();
+
+        var trigger = triggerCreator(jobKey, schedulingValue, key);
+
+        var time = await _scheduler.ScheduleJob(jobDetail, trigger, cancellationToken);
+        logger.LogDebug("[{Name}] finished, {Time}", nameof(ScheduleJob), time);
+    }
+    
+    private string GetIdentity(string jobKey)
+    {
+        return $"{jobKey}-Trigger";
+    }
+
+    private ITrigger CreateTrigger(string jobKey, int minutes, JobKey key)
+    {
+        return TriggerBuilder.Create()
+            .WithIdentity(new TriggerKey(GetIdentity(jobKey)))
+            .WithSimpleSchedule(x => x.WithIntervalInMinutes(minutes).RepeatForever())
+            .ForJob(key)
+            .Build();
+    }
+
+    private ITrigger CreateTrigger(string jobKey, string expressionOrInterval, JobKey key)
+    {
+        return TriggerBuilder.Create()
+            .WithIdentity(new TriggerKey(GetIdentity(jobKey)))
+            .WithCronSchedule(expressionOrInterval, x => x.InTimeZone(TimeZoneInfo.Local))
+            .ForJob(key)
+            .Build();
+    }
+}
---
+++ Scheduler/Services/SchedulerFactoryService.cs
@@ -0,0 +1,107 @@
+﻿// Copyright (C) 2024 Maxim [maxirmx] Samsonov (www.sw.consulting)
+// All rights reserved.
+// This file is a part of OiltrackGateway applcation
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
+// BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+using Microsoft.Extensions.Logging;
+using Quartz;
+
+namespace Refactored.Waffle.Scheduler.Services;
+
+internal sealed class SchedulerFactoryService(
+    ISchedulerFactory factory, ILogger<SchedulerFactoryService> logger) : ISchedulerFactoryService
+{
+    private IScheduler? _scheduler;
+    private bool _isInitialized;
+
+    private bool _initInProgress;
+    private static readonly object InitLock = new();
+
+    public async Task<IScheduler> GetScheduler(CancellationToken cancellationToken)
+    {
+        if (!_isInitialized)
+        {
+            await WaitInitialize(cancellationToken);
+        }
+        return _scheduler!;
+    }
+
+    private async Task WaitInitialize(CancellationToken cancellationToken)
+    {
+        var tryInit = 5;
+        const int delayTime = 500;
+        while (tryInit > 0)
+        {
+            if (!_isInitialized)
+            {
+                await Initialize(cancellationToken);
+                if (!_isInitialized)
+                {
+                    await Task.Delay(delayTime, cancellationToken);
+                    if (!_isInitialized)
+                    {
+                        tryInit--;
+                        continue;
+                    }
+                }
+            }
+
+            break;
+        }
+    }
+
+    private async Task Initialize(CancellationToken cancellationToken)
+    {
+        lock (InitLock)
+        {
+            if (_initInProgress)
+            {
+                return;
+            }
+            _initInProgress = true;
+        }
+
+        if (_isInitialized)
+        {
+            logger.LogDebug("[{Name}]: Scheduler is already initializing", nameof(SchedulerFactoryService));
+            return;
+        }
+
+        try
+        {
+            logger.LogDebug("[{Name}]: Initialize started", nameof(SchedulerFactoryService));
+            _scheduler ??= await factory.GetScheduler(cancellationToken);
+            await _scheduler.Start(cancellationToken);
+
+            logger.LogDebug("[{Name}]: Initialize finished", nameof(SchedulerFactoryService));
+            _isInitialized = true;
+            _initInProgress = false;
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "[{Name}]: Initialize error", nameof(SchedulerFactoryService));
+            _initInProgress = false;
+            throw;
+        }
+    }
+}
